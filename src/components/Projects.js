55084c6847a197d1b1d1d0a8962c72e0e76cd3e3
Shaurya-dub <shauryadubey@hotmail.ca>
2022-10-11 14:40:17 -0400

    added ProjectCard component

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,9 @@
 import { Container, Col, Row, Nav, Tab } from "react-bootstrap";
-
+import { ProjectCard } from "./ProjectCard";
+import colorSharp2 from "../assets/img/color-sharp2.png"
+import projImg1 from "../assets/img/project-img1.png"
+import projImg2 from "../assets/img/project-img2.png"
+import projImg3 from "../assets/img/project-img3.png"
 export const Projects = () => {
   const projects = [
     {
@@ -58,24 +62,21 @@ export const Projects = () => {
                 </Nav.Item>
               </Nav>
               <Tab.Content>
-                <Tab.Pane eventKey='first'>
-                    <Row>
-                        {
-                            projects.map((project,index) => {
-                                return(
-                                    <p>{project.title}</p>
-                                )
-                            })
-                        }
-                    </Row>
+                <Tab.Pane eventKey="first">
+                  <Row>
+                    {projects.map((project, index) => {
+                      return <ProjectCard key={index} {...project} />;
+                    })}
+                  </Row>
                 </Tab.Pane>
-                <Tab.Pane eventKey='second'>lorem Ipsum 2</Tab.Pane>
-                <Tab.Pane eventKey='third'>lorem Ipsum 3</Tab.Pane>
+                <Tab.Pane eventKey="second">lorem Ipsum 2</Tab.Pane>
+                <Tab.Pane eventKey="third">lorem Ipsum 3</Tab.Pane>
               </Tab.Content>
             </Tab.Container>
           </Col>
         </Row>
       </Container>
+      <img classNAme="background-image-right" src={colorSharp2}></img>
     </section>
   );
 };
