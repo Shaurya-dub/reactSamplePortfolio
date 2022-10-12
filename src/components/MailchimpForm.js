4587c889619d1d5e994493b3d035f723f870f50a
Shaurya-dub <shauryadubey@hotmail.ca>
2022-10-12 15:57:01 -0400

    Mailchimp form done

--- src/components/MailchimpForm.js
+++ src/components/MailchimpForm.js
@@ -1,4 +1,5 @@
 import MailchimpSubscribe from "react-mailchimp-subscribe";
+import { NewsLetter } from "./Newsletter";
 export const MailchimpForm = () => {
     const postUrl = `${process.env.REACT_APP_MAILCHIMP_URL}?u=${process.env.REACT_APP_MAILCHIMP_U}$id=${process.env.REACT_APP_MAILCHIMP_ID}`
     return (
@@ -6,7 +7,11 @@ export const MailchimpForm = () => {
         <MailchimpSubscribe
         url={postUrl}
         render={({subscribe, status, message}) => (
-            <> </>
+            <NewsLetter
+            status={status}
+            message={message}
+            onValidated={formData => subscribe(formData)}
+            />
         )} />
         </>
     )
