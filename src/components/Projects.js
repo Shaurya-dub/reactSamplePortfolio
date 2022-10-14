de142ded7161180cfe3498f8fcdafe3369a4be65
Shaurya-dub <shauryadubey@hotmail.ca>
2022-10-14 12:47:53 -0400

    done

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,6 +4,8 @@ import colorSharp2 from "../assets/img/color-sharp2.png"
 import projImg1 from "../assets/img/project-img1.png"
 import projImg2 from "../assets/img/project-img2.png"
 import projImg3 from "../assets/img/project-img3.png"
+import "animate.css";
+import TrackVisibility from "react-on-screen";
 export const Projects = () => {
   const projects = [
     {
@@ -42,6 +44,9 @@ export const Projects = () => {
       <Container>
         <Row>
           <Col>
+          <TrackVisibility>
+            {({isVisible}) => 
+            <div className={isVisible ? "animate__animated animate__bounce" : ''}>
             <h2>Projects</h2>
             <p>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, nisi
@@ -49,6 +54,8 @@ export const Projects = () => {
               dolorum molestias est? Aperiam saepe voluptatibus dignissimos
               consequatur nobis, vero natus!
             </p>
+            </div>}
+            </TrackVisibility>
             <Tab.Container id="projects-tabs" defaultActiveKey="first">
               <Nav variant="pills" className='nav-pills mb-5 justify-content-center align-items-center' id="pills-tab">
                 <Nav.Item>
