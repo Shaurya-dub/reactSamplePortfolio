d6395c9457e5bb25d3d7ad9d7f07c7598c09697f
Shaurya-dub <shauryadubey@hotmail.ca>
2022-10-14 12:38:29 -0400

    added visibility functionality

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { ArrowRightCircle } from "react-bootstrap-icons";
+import 'animate.css'
+import TrackVisibility from "react-on-screen";
 import headerImg from "../assets/img/header-img.svg";
 
 export const Banner = () => {
@@ -51,6 +53,9 @@ export const Banner = () => {
       <Container>
         <Row className="align-items-center">
           <Col xs={12} md={6} xl={7}>
+            <TrackVisibility>
+            {({isVisible}) => 
+            <div className={isVisible ? "animated__animate animate__fadeIn" : ''}>
             <span className="tagline">Welcome to my Portfolio</span>
             <h1>
               {`Hi, I'm Shaurya`} <span className="wrap">{text}</span>
@@ -64,6 +69,8 @@ export const Banner = () => {
             <button onClick={() => console.log("connect")}>
               Let's Connect <ArrowRightCircle size={25} />{" "}
             </button>
+            </div>}
+            </TrackVisibility>
           </Col>
           <Col xs={12} md={6} xl={5}>
             <img src={headerImg} alt="Header Img" />
